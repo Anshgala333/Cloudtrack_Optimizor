50c4172ff2cbe0afafc3626a7a11de30ec678dd1
Anshgala333 <anshgala333@gmail.com>
2025-07-14 15:44:52 +0530

    added all host

--- main.py
+++ main.py
@@ -30,4 +30,4 @@ def greet():
 app.include_router(upload)
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", port=4000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=4000, reload=True)
